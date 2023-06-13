68e9e58ed50972d4083d610752d784c1eabc524b
Ibuki <c0b2213810@edu.teu.ac.jp>
2023-06-13 16:18:12 +0900

    練習問題6

--- pygame_image.py
+++ pygame_image.py
@@ -12,17 +12,24 @@ def main():
     kk_img2 = pg.transform.rotozoom(kk_img,10,1.0)
     kk_imgs = [kk_img,kk_img2]
     tmr = 0
+    x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img,[1600-x,0])
         screen.blit(kk_imgs[tmr%2],(300,200))
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1 
+        x += 1       
+        if x >= 1600:
+            x = 0
+        
+            
 
-
+        clock.tick(100)
 if __name__ == "__main__":
     pg.init()
     main()
