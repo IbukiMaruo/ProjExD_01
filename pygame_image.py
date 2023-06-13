740d6f930217825c272f683fdff4fdf1ff531e80
Ibuki <c0b2213810@edu.teu.ac.jp>
2023-06-13 21:23:17 +0900

    演習問題

--- pygame_image.py
+++ pygame_image.py
@@ -8,27 +8,27 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img,True,False)
-    kk_img = pg.image.load("ex01/fig/3.png")
-    kk_img = pg.transform.flip(kk_img,True,False)
-    kk_img2 = pg.transform.rotozoom(kk_img,10,1.0)
-    kk_imgs = [kk_img,kk_img2]
+    KK_img = pg.image.load("ex01/fig/3.png")
+    KK_img2 = pg.transform.flip(KK_img,True,False)
+    KK_img3 = pg.transform.rotozoom(KK_img2, 10, 1.0)
+    KK_imgs = [KK_img2,KK_img3]
     tmr = 0
     x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img2,[1600-x,0])
-        screen.bilt(bg_img,[3200-x,0])
-        screen.blit(kk_imgs[tmr//100%2],(300,200))
+        screen.blit(bg_img, [-x,0])
+        screen.blit(bg_img2, [1600-x,0])
+        screen.blit(bg_img,[3200-x,0])
+        screen.blit(KK_imgs[tmr//100%2],[300,200])
         pg.display.update()
-        tmr += 1 
-        x += 1       
-        if x >= 1600:
-            x = 0
+        tmr += 1  
+        x += 1
         clock.tick(100)
+        if x > 1599:
+            x = 0
+
 if __name__ == "__main__":
     pg.init()
     main()
